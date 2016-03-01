f41181052a00c11648096fc7c6a8d0f91bfae90c
t3rmin41 <t33rmin41@gmail.com>
2016-03-01 21:30:36 +0200

    Initial commit of MyServlet

---
+++ src/com/nikzgs/servlet/MyServlet.java
@@ -0,0 +1,24 @@
+package com.nikzgs.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 4390449198245269965L;
+
+    public MyServlet() {
+        super();
+    }
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println("MyServlet says \"Hello!\"");
+    }
+}
